ce3753c986c18ef7272be089c36ceb520174acd0
UltraHispeed-Elite <62296749+UltraHispeed-Elite@users.noreply.github.com>
2024-01-26 10:07:38 -0600

    Shading_engine
    so the lighting engine works, but it works by changing the opacity of the individual sprites, this combined with mass amounts of sprites generates lag, so instead I've create a sprite that overlaps with the entire screen with the opacity changing based on the player's direction.

--- sketch.js
+++ sketch.js
@@ -1,158 +1,83 @@
 var player;
-var enemy;
+var screen;
+var screen2;
 
-var light_source = [
-    [0,0,400,400],
-    [0,400,400,800],
-    [400,0,800,400],
-    [400,400,800,800]
-];
+var img1;
 
-var light_sprites;
-
-var xc = [];
-var yc = [];
-var c;
-var d = [];
-var e = [];
-var g = [];
-var h = [];
-var vx = [];
-var vy = [];
-var v = [];
-var cv = [];
-var dave = 0;
+var e,f,g,h;
 
 function setup() {
-  createCanvas(800,800);
-  
-  player = new Sprite(300,300,64,64);
-  player.rotationLock = true;
-  player.img = 'ghost2.png';
-  
-  enemy = new Group();
-  enemy.rotationLock = true;
-  enemy.img = 'ghost2.png';
-  
-  for(let i=0; i<1; i++){
-    new enemy.Sprite(200+(i*5),200+(i*5),50,50);
-  }
+    createCanvas(400,400);
 
-  light_sprites = new Group();
+    player = new Sprite(200,200,50,50);
+    player.color='red';
+    player.img = 'ghost2.png';
 
-  for(let i=0; i<light_source.length; i++){
-    new light_sprites.Group();
-  }
+    img1 = new Sprite(200,200,400,400);
+    img1.collider = 'n';
+    img1.img = 'hello.png';
 }
 
-function draw(){
-  background("black");
-  
-//   if(player.x >= 0 && player.x <= 400){
-//     if(player.y >= 0 && player.y <= 400){
-//         createLight(0,0,400,400,1);
-//     }else if(player.y >= 400 && player.y <= 800){
-//         createLight(0,400,400,800,2);
-//     }
-//   }else if(player.x >= 400 && player.x <= 800){
-//     if(player.y >= 0 && player.y <= 400){
-//         createLight(400,0,800,400,3);
-//     }else if(player.y >= 400 && player.y <= 800){
-//         createLight(400,400,800,800,4);
-//     }
-//   }
+function draw() {
+    background("purple");
 
-    for(let i=0; i<light_source.length; i++) {
-        for(let j=0; j<allSprites.length; j++) {
-            if(allSprites[j].x >= light_source[i][0] && allSprites[j].x <= light_source[i][2]){
-                if(allSprites[j].y >= light_source[i][1] && allSprites[j].y <= light_source[i][3]){
-                    createLight(light_source[i][0],light_source[i][1],light_source[i][2],light_source[i][3],i);
-                    spriteLight(j, i);
-                }
-            }
-        }
-    }
-
-  playerMovement();
-}
-
-function createLight(x1, y1, x2, y2, id){
-  xc.length = light_source.length;
-  yc.length = light_source.length;
-  xc[id] = (x1+x2)/2;
-  yc[id] = (y1+y2)/2;
-}
-
-function spriteLight(a,b){
-  d.length = allSprites.length;
-  e.length = allSprites.length;
-  g.length = allSprites.length;
-  h.length = allSprites.length;
-  vx.length = allSprites.length;
-  vy.length = allSprites.length;
-  v.length = allSprites.length;
-  cv.length = allSprites.length;
-    
-  for(let i = 0; i<allSprites.length; i++) {
-    d[i]=0;
-    e[i]=0;
-    g[i]=0;
-    h[i]=0;
-    vx[i]=0;
-    vy[i]=0;
-    v[i]=0;
-    cv[i]=0;
-    data(a,b);
-  }
-}
-
-function data(a,b) {
-  if(allSprites[a].x < xc[b]) {
-    d[a] = xc[b] - allSprites[a].x;
-    e[a] = (d[a]/xc[b])*100;
-  }else if(allSprites[a].x > xc[b]) {
-    d[a] = allSprites[a].x - xc[b];
-    e[a] = (d[a]/xc[b])*100;
-  }else {
-    d[a]=0;
-    e[a]=0;
-  }
-    
-  if(allSprites[a].y < yc[b]) {
-    g[a] = yc[b] - allSprites[a].y;
-    h[a] = (g[a]/yc[b])*100;
-  }else if(allSprites[a].y > yc[b]) {
-    g[a] = allSprites[a].y - yc[b];
-    h[a] = (g[a]/yc[b])*100;
-  }else {
-    g[a]=0
-    h[a]=0;
-  }
-    
-  vx[a] = 100-e[a];
-  vy[a] = 100-h[a];
-  v[a] = (vx[a]+vy[a])/2
-  cv[a] = v[a]/100;
-  //colorMode = (HSB, 255)
-  //allSprites[a].color = (255,255,cv[a]);
-  if(cv[a] < 0) {
-    cv[a] = 0;
-  }
-  allSprites[a].opacity = cv[a];
-  // console.log(cv);
+    playerMovement();
+    shade_engine();
 }
 
 function playerMovement() {
-  if(kb.pressing("w")) {
-    player.y -= 5;
-  }
-  if(kb.pressing("a")) {
-    player.x -= 5;
-  }
-  if(kb.pressing("s")) {
-    player.y += 5;
-  }
-  if(kb.pressing("d")) {
-    player.x += 5;
-  }
+    if(kb.pressing('w')) {
+        player.y -= 5;
+    }
+
+    if(kb.pressing('a')) {
+        player.x -= 5;
+    }
+
+    if(kb.pressing('s')) {
+        player.y += 5;
+    }
+
+    if(kb.pressing('d')) {
+        player.x += 5;
+    }
+}
+
+function shade_engine() {
+    if(player.x<img1.x){
+        e = img1.x - player.x;
+        f = (e/img1.x)*100;
+    }else if(player.x>img1.x) {
+        e = player.x - img1.x;
+        f = (e/img1.x)*100;
+    }else {
+        e = 0;
+        f = 0;
+    }
+
+    if(player.y<img1.y){
+        g = img1.y - player.y;
+        h = (g/img1.y)*100;
+    }else if(player.y>img1.y) {
+        g = player.y - img1.y;
+        h = (g/img1.y)*100;
+    }else {
+        g = 0;
+        h = 0;
+    }
+
+    let vx = 100-f;
+    let  vy = 100-h;
+    let v = (vx+vy)/2
+    let cv = v/100;
+
+    if(cv < 0){
+        cv = 0;
+    }else if(cv > 0.9) {
+        cv = 1;
+    }
+
+    img1.opacity = 1-cv;
+
+    console.log(img1.opacity);
 }
