d8e014532f7d6d9a1150756762139e0fa15cfc00
UltraHispeed-Elite <62296749+UltraHispeed-Elite@users.noreply.github.com>
2024-01-26 14:14:31 -0600

    Add files via upload
    updated the system to have multiple points that are determined as "light sources" and the program calculates the distance and figures out which light source is the closest one to the player and set's the opacity of the overlay system based on the position of the player from the closest light source available

--- sketch.js
+++ sketch.js
@@ -1,66 +1,115 @@
 var player;
-var screen;
-var screen2;
 
-var img1;
+var img_v;
 
-var e,f,g,h;
+var lightPoint = [
+    [266, 294],
+    [507,659]
+];
+
+var vals = [];
+vals.length = lightPoint.length;
+
+var closest_light = 0;
+
+var pointCount = 0;
 
 function setup() {
-    createCanvas(400,400);
+  createCanvas(800,800);
+  
+  player = new Sprite(200,200,50,50);
+  player.img = 'ghost2.png'
 
-    player = new Sprite(200,200,50,50);
-    player.color='red';
-    player.img = 'ghost2.png';
-
-    img1 = new Sprite(200,200,400,400);
-    img1.collider = 'n';
-    img1.img = 'hello.png';
+  img_v = new Sprite(400,400,800,800);
+  img_v.collider = 'n'
+  img_v.img = 'hello1.png';
 }
 
 function draw() {
-    background("purple");
-
-    playerMovement();
-    shade_engine();
+  background('purple');
+  
+  player_movement();
+  find_shortest();
+  shade_engine();
+  console.log(vals, closest_light);
 }
 
-function playerMovement() {
-    if(kb.pressing('w')) {
-        player.y -= 5;
-    }
+function player_movement(){
+  if(kb.pressing("w")) {
+    player.y -= 5;
+  }
+  
+  if(kb.pressing("a")) {
+    player.x -= 5;
+  }
+  
+  if(kb.pressing("s")) {
+    player.y += 5;
+  }
+  
+  if(kb.pressing("d")) {
+    player.x += 5;
+  }
+}
 
-    if(kb.pressing('a')) {
-        player.x -= 5;
+function find_shortest() {
+  for(let i=0; i<lightPoint.length; i++) {
+    calculate_distance(i);
+    for(let j=0; j<lightPoint.length; j++) {
+      if(j===i) {
+        //console.log("same light point");
+      }else {
+        if(vals[j] > vals[i]){
+          pointCount += 1;
+          if(pointCount === (lightPoint.length-1)) {
+            closest_light = i;
+            console.log(closest_light);
+          }
+        }else {
+            pointCount = 0;
+          }
+      }
     }
+    }
+}
 
-    if(kb.pressing('s')) {
-        player.y += 5;
-    }
-
-    if(kb.pressing('d')) {
-        player.x += 5;
-    }
+function calculate_distance(a) {
+  let val1, val2, val3;
+  
+  if(player.x > lightPoint[a][0]) {
+    val1 = player.x - lightPoint[a][0];
+  }else {
+    val1 = lightPoint[a][0] - player.x;
+  }
+  
+  if(player.y > lightPoint[a][1]) {
+    val2 = player.y - lightPoint[a][1];
+  }else {
+    val2 = lightPoint[a][1] - player.y;
+  }
+  
+  val3 = (val1+val2)/2;
+  vals[a] = val3;
 }
 
 function shade_engine() {
-    if(player.x<img1.x){
-        e = img1.x - player.x;
-        f = (e/img1.x)*100;
-    }else if(player.x>img1.x) {
-        e = player.x - img1.x;
-        f = (e/img1.x)*100;
+    if(player.x<lightPoint[closest_light][0]){
+        e = lightPoint[closest_light][0] - player.x;
+        f = (e/lightPoint[closest_light][0])*100;
+    }else if(player.x>lightPoint[closest_light][0]) {
+        e = player.x - lightPoint[closest_light][0];
+        f = (e/lightPoint[closest_light][0])*100;
     }else {
         e = 0;
         f = 0;
     }
 
-    if(player.y<img1.y){
-        g = img1.y - player.y;
-        h = (g/img1.y)*100;
-    }else if(player.y>img1.y) {
-        g = player.y - img1.y;
-        h = (g/img1.y)*100;
+    if(player.y<lightPoint[closest_light][1]){
+        g = lightPoint[closest_light][1] - player.y;
+        h = (g/lightPoint[closest_light][1])*100;
+    }else if(player.y>lightPoint[closest_light][1]) {
+        g = player.y - lightPoint[closest_light][1];
+        h = (g/lightPoint[closest_light][1])*100;
     }else {
         g = 0;
         h = 0;
@@ -77,7 +126,7 @@ function shade_engine() {
         cv = 1;
     }
 
-    img1.opacity = 1-cv;
+    img_v.opacity = 1-cv;
 
-    console.log(img1.opacity);
+    console.log(img_v.opacity);
 }
